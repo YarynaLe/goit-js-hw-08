5230a38a3d5eae943ab56148b4769c6f6e1e8e98
YarynaLe <109382584+YarynaLe@users.noreply.github.com>
2023-01-16 22:50:57 +0200

    +

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,58 +1,6 @@
 
 import throttle from 'lodash.throttle';
 
-// const STORAGE_KEY = 'feedback-form-state';
-// const formData = {};
-
-// const refs = {
-//   form: document.querySelector('.feedback-form'),
-//   textarea: document.querySelector('.feedback-form textarea'),
-//   email: document.querySelector('.feedback-form input'),
-// };
-
-// refs.form.addEventListener('submit', onFormSubmit);
-// refs.form.addEventListener('input', throttle(onTextareaInput, 500));
-
-
-// function onTextareaInput(event) {
-//   const formName = event.target.name;
-//   const formValue = event.target.value;
-
-//   formData[formName] = formValue;
-
-//   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-// }
-
-// populatedTextArea();
-
-// function populatedTextArea() {
-//   const savedFormData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-
-//   if (savedFormData) {
-//     refs.email.value = savedFormData.email;
-//     refs.textarea.value = savedFormData.message;
-//   }
-// }
-
-//  function onFormSubmit(event) {
-//   event.preventDefault();
-//   if (formData.email === '' || formData.message === '') {
-//     alert('Заповніть всі поля!');
-//     return
-//   }
-//   event.preventDefault();
-//   event.currentTarget.reset();
-//   localStorage.removeItem(STORAGE_KEY);
-//  }
-
-
-// function onFormSubmit(event) {
-//   event.preventDefault();
-//   event.currentTarget.reset();
-//   localStorage.removeItem(STORAGE_KEY);
-// }
-
-
 const LOCALSTORAGE_KEY = 'feedback-form-state';
 
 const refs = {
